ccc55e464d0b3c401e132ade962465efddde680e
DiegoPertierraM <diegopmenendez@gmail.com>
2024-02-21 19:21:56 +0100

    Final update

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,25 @@ const sendBtn = document.querySelector('.send-btn');
 const form = document.querySelector('form');
 const selectPizzaSize = document.querySelector('#pizza-size');
 const selectBreadOptions = document.querySelector('#bread-options');
-const pepperoni = document.querySelector('#pepperoni');
-const mushrooms = document.querySelector('#mushrooms');
-const onion = document.querySelector('#onion');
-const pepper = document.querySelector('#pepper');
-const pineapple = document.querySelector('#pineapple');
 const result = document.querySelector('.result');
-const checkboxValues = [];
-
-document.querySelectorAll('.custom-checkbox').forEach((checkbox) => {
-  if (checkbox.checked === true) {
-    console.log(true);
-  }
-});
 
 const onSubmit = (event) => {
+  const checkboxValues = [];
   event.preventDefault();
   const pizzaSize = selectPizzaSize.value;
   const breadOptions = selectBreadOptions.value;
-  result.innerHTML = `Has pedido una pizza ${pizzaSize} de masa ${breadOptions}
-  con: `;
-  console.log(checkboxValues);
+  document.querySelectorAll('.custom-checkbox').forEach((checkbox) => {
+    if (checkbox.checked === true) {
+      checkboxValues.push(checkbox.name);
+    }
+  });
+  if (checkboxValues.length >= 1) {
+    result.innerHTML = `Has pedido una pizza ${pizzaSize} con masa ${breadOptions}
+  con los siguientes ingredientes: ${checkboxValues.join(', ')}`;
+  } else {
+    result.innerHTML = 'Debes introducir al menos 1 ingrediente!';
+  }
+  sendBtn.value = 'Pedir otra pizza';
 };
 
 sendBtn.addEventListener('click', onSubmit);
