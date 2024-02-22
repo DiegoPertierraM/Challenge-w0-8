a2680e50bfdd2b356c9a2c36065c2b5e6f6ebc8d
DiegoPertierraM <diegopmenendez@gmail.com>
2024-02-22 10:41:11 +0100

    Add corrections

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,27 +1,28 @@
 export const orderPizza = () => {
   const sendBtn = document.querySelector('.send-btn');
-  const selectPizzaSize = document.querySelector('#pizza-size');
-  const selectBreadOptions = document.querySelector('#bread-options');
-  const result = document.querySelector('.result');
 
-  const onSubmit = (event) => {
+  const onClick = (event) => {
+    const resultText = document.querySelector('.result');
     const checkboxValues = [];
     event.preventDefault();
-    const pizzaSize = selectPizzaSize.value;
-    const breadOptions = selectBreadOptions.value;
+    const pizzaSize = document.querySelector('#pizza-size').value;
+    const breadOptions = document.querySelector('#bread-options').value;
+
     document.querySelectorAll('.checkbox').forEach((checkbox) => {
       if (checkbox.checked === true) {
         checkboxValues.push(checkbox.name);
       }
     });
+
     if (checkboxValues.length >= 1) {
-      result.innerHTML = `Has pedido una pizza ${pizzaSize} con masa ${breadOptions}
+      resultText.innerHTML = `Has pedido una pizza ${pizzaSize} con masa ${breadOptions}
   con los siguientes ingredientes: ${checkboxValues.join(', ')}`;
       sendBtn.value = 'Pedir otra pizza';
     } else {
-      result.innerHTML = 'Debes introducir al menos 1 ingrediente!';
+      resultText.innerHTML = 'Debes introducir al menos 1 ingrediente!';
     }
-    result.removeAttribute('hidden');
+    resultText.removeAttribute('hidden');
   };
-  sendBtn.addEventListener('click', onSubmit);
+
+  sendBtn.addEventListener('click', onClick);
 };
